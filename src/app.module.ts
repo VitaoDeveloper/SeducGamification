46dfcb5ffadefb306211c1f809bd07aa0146ac27
VitaoDeveloper <paulo.borges.prates@gmail.com>
2026-09-23 12:54:25 -0300

    feat: cria modulo global do Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { PrismaService } from './modules/prisma/prisma.service.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -14,6 +15,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'seduc_gamification',
     }),
+    PrismaService
   ],
   controllers: [AppController],
   providers: [AppService],
